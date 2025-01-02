1f876471037c353291a2afdd47e2c30aaf827f26
Q-verse <officialqverse@gmail.com>
2025-01-02 08:09:36 +0000

    new math numbers seting

---
+++ 01_basic/nums-and-math.js
@@ -0,0 +1,27 @@
+const score = 400
+//console.log(score);
+const balance = new Number(100)
+
+//console.log(balance.toString().length);
+
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString());
+
+
+///----------------math=-----------------
+
+
+//console.log(Math.abc(-4));
+//console.log(Math.round(4.2));
+//console.log(Math.ceil(4.3));
+//console.log(Math.floor(4.1));
+//console.log(Math.min(4, 3, 5, 9));
+//console.log(Math.max(4, 3, 5, 9));
+
+console.log(Math.random())
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
