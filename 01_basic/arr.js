9446a8724a43bf65503faa337e4c256bc7c3fcff
Q-verse <officialqverse@gmail.com>
2025-01-02 10:14:50 +0000

    some arry

---
+++ 01_basic/arr.js
@@ -0,0 +1,41 @@
+const myarr = [0,1,2,3,4]
+const myhero = ['mom' , 'mycomputer'];
+
+const myarr2 = new Array(1 ,2, 3, 4);
+//console.log(myarr[0]);
+
+
+myarr.push(8)
+//console.log(myarr.toString());
+
+//myarr.unshift(9)
+// myarr.shift()
+
+//console.log(myarr)
+
+
+console.log(myarr.includes(9));
+console.log(myarr.indexOf(3));
+
+const newarr = myarr.join();
+
+console.log(myarr);
+console.log(newarr);
+
+//flat (infinity) you have to get learn docs to get better get understand 
+//concat
+
+
+/*converting arry use this :-
+console.log(Arry.frome("Abhishek"))*/
+
+
+/*
+best more example :- 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+use this code are output is [100, 200, 300] OK */ 
